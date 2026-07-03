aba52b1e12e8546a4fd77d5577f14eb266206519
taynpg <taynpg@163.com>
2026-07-03 14:33:58 +0800

    fix：类型注册。

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char* argv[])
     // Qt5兼容
     qRegisterMetaType<int16_t>("int16_t");
     qRegisterMetaType<FramePtr>("FramePtr");
+    qRegisterMetaType<std::vector<FileMeta>>("std::vector<FileMeta>");
 
     if (argc == 2 && std::strcmp(argv[1], "--version") == 0) {
         std::cout << VERSION_GIT_COMMIT << "-v" << VERSION_NUM << "-" << VERSION_DEV << std::endl;
